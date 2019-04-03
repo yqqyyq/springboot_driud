d3a7bd6ea3180100d806b5507cab1702b699fe23
yqqyyq <yqssg@qq.com>
2019-04-03 21:55:07 +0800

    update
    加入自定义starter

--- src/main/java/com/xxx/controller/SystemController.java
+++ src/main/java/com/xxx/controller/SystemController.java
@@ -1,8 +1,11 @@
 package com.xxx.controller;
 
+import com.yqqyyq.stat.HelloServiceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 页面跳转
@@ -10,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class SystemController {
+
+    @Autowired
+    private HelloServiceConfiguration helloService ;
+
 
     @RequestMapping(value = "/")
     public String index() {
@@ -30,4 +37,17 @@ public class SystemController {
     public String page(@PathVariable("module") String module, @PathVariable("url") String url) {
         return module + "/" + url;
     }
+
+    @RequestMapping(value = "/com/yqqyyq/name")
+    @ResponseBody
+    public String yqname() {
+        return helloService.getName();
+    }
+
+    @RequestMapping(value = "/com/yqqyyq/hobby")
+    @ResponseBody
+    public String yqhobby() {
+        return helloService.getHobby();
+    }
+
 }
